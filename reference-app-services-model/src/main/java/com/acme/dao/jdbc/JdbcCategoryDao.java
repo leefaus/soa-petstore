dcc555dee0fa91f10d8d2c8d104da020485e13f8
Scott Frederick <scottyfred@gmail.com>
2012-06-21 10:51:08 -0500

    Added initialization of product table. Replaced JdbcDao with a Spring-provided support class.

--- reference-app-services-model/src/main/java/com/acme/dao/jdbc/JdbcCategoryDao.java
+++ reference-app-services-model/src/main/java/com/acme/dao/jdbc/JdbcCategoryDao.java
@@ -3,22 +3,28 @@ package com.acme.dao.jdbc;
 import com.acme.dao.CategoryDao;
 import com.acme.mapper.CategoryRowMapper;
 import com.acme.model.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Repository("categoryDao")
-public class JdbcCategoryDao extends JdbcDao implements CategoryDao {
+public class JdbcCategoryDao extends NamedParameterJdbcDaoSupport implements CategoryDao {
+    @Autowired
+    public JdbcCategoryDao(DataSource dataSource) {
+        setDataSource(dataSource);
+    }
+
     public List<Category> findAllCategories() {
         String sql = "SELECT * FROM CATEGORY";
-        List<Category> categories = jdbcTemplate.query(sql, (SqlParameterSource) null, new CategoryRowMapper());
-        return categories;
+        return getNamedParameterJdbcTemplate().query(sql, (SqlParameterSource) null, new CategoryRowMapper());
     }
 
     public Category findCategoryById(String id) {
         String sql = "SELECT * FROM CATEGORY WHERE id='" + id + "'";
-        Category category = jdbcTemplate.queryForObject(sql, (SqlParameterSource) null, new CategoryRowMapper());
-        return category;
+        return getNamedParameterJdbcTemplate().queryForObject(sql, (SqlParameterSource) null, new CategoryRowMapper());
     }
 }
