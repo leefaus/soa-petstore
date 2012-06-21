8b967954aa0cd7f40d03114eb8f7b20efffbaf92
Scott Frederick <scottyfred@gmail.com>
2012-06-20 22:48:26 -0500

    Moved Service interface and ServiceImpl classes from the services-model project to the services-web project.

--- reference-app-services-model/src/main/java/com/acme/dao/jdbc/JdbcCategoryDao.java
+++ reference-app-services-model/src/main/java/com/acme/dao/jdbc/JdbcCategoryDao.java
@@ -3,6 +3,7 @@ package com.acme.dao.jdbc;
 import com.acme.dao.CategoryDao;
 import com.acme.mapper.CategoryRowMapper;
 import com.acme.model.Category;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,13 +12,13 @@ import java.util.List;
 public class JdbcCategoryDao extends JdbcDao implements CategoryDao {
     public List<Category> findAllCategories() {
         String sql = "SELECT * FROM CATEGORY";
-        List<Category> categories = jdbcTemplate.query(sql, new CategoryRowMapper());
+        List<Category> categories = jdbcTemplate.query(sql, (SqlParameterSource) null, new CategoryRowMapper());
         return categories;
     }
 
     public Category findCategoryById(String id) {
         String sql = "SELECT * FROM CATEGORY WHERE id='" + id + "'";
-        Category category = jdbcTemplate.queryForObject(sql, new CategoryRowMapper());
+        Category category = jdbcTemplate.queryForObject(sql, (SqlParameterSource) null, new CategoryRowMapper());
         return category;
     }
 }
